02c9b011c1a640573048fea55b46b644d4da6828
Rahma Sari <155930474+rahmasarinawir@users.noreply.github.com>
2024-01-11 16:00:45 +0000

    First Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed= new Typed(".teks", {
+    strings: ["Seorang Mahasiswa", "Sedang Belajar Pemograman"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
